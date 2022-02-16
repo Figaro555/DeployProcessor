af5d1f5cf57894882c6e36f5706ae054affabc79
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2022-02-16 13:24:45 +0200

    modified channel sender to step function usage

--- MChannelSender1/index.py
+++ MChannelSender1/index.py
@@ -3,25 +3,23 @@ import boto3
 import math
 import json
 from YouTubeChannels import *
+from datetime import datetime
+import time
 
 
 def lambda_handler(event, context):
-    threads_num = 5
-    n = math.ceil(len(my_channels_id) / threads_num)
+    threads_num = 4
+    n = math.ceil(len(my_channels_id)/threads_num)
     c = chunks(my_channels_id, n)
-
-    client = boto3.client("sqs")
-    for i, ch in enumerate(c):
-        client.send_message(
-            QueueUrl='https://sqs.us-east-1.amazonaws.com/062261762656/MSQSChannel',
-            MessageBody=(json.dumps({"part": i, "array": ch}))
-        )
+    t = datetime.now()
+    l = list(range(0, threads_num))
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': {"array": c, "hour": str(t.hour), "date": str(datetime.date(t)), "indexes": l, "time": str(datetime.fromtimestamp(int(time.time())))}
     }
 
 
 def chunks(l, n):
     n = max(1, n)
-    return [l[i:i + n] for i in range(0, len(l), n)]
+    return [l[i:i+n] for i in range(0, len(l), n)]
