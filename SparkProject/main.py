c23939e8cb500e200e65e4f51ef0c9c962b617ed
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2022-04-18 20:12:38 +0300

    modified main for glue

--- SparkProject/main.py
+++ SparkProject/main.py
@@ -1,18 +1,25 @@
-from datetime import datetime
-
-from pyspark.sql import SparkSession
-
-from Executor.Executor import Executor
-
-
-def main():
-    path_to_save = '/home/admin1/Рабочий стол/SparkProject1/test'
-    start_time = datetime.now()
-    ss = SparkSession.builder.master("local[*]").getOrCreate()
-    executor = Executor()
-    executor.execute(ss, path_to_save)
-    print(datetime.now() - start_time)
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from SparkProject.Executor.Executor import Executor
 
 
-if __name__ == '__main__':
-    main()
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+path_to_save = 's3://mbucket111111/Test'
+
+executor = Executor()
+executor.execute(spark, path_to_save)
+
+job.commit()
